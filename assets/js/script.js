aa785467c688d92586bf2af711a140c1d8d22f6b
Ravi Velaga <rvelaga@opentable.com>
2022-07-10 14:50:17 +1000

    removed redundant code

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,23 +5,10 @@
 
 //Global Variable
 var teamSearch = $("#search-value").val();
-var savedSearches = [];
-var searchList = $(".history");
 
-// Local Storage for usuer input
-
-function saveSearch() {
-    localStorage.setItem("search-value", teamSearch);
-    console.log(saveSearch);
-}
-function renderPastSearchHistory() {
-    var lastSearch = localStorage.getItem("search-value");
-    console.log(lastSearch);
-}
 
 $("#search-button").on("click", function () {
     teamSearch = $("#search-value").val();
-
     getTeamOverview(teamSearch);
     $("#search-value").val("");
 });
@@ -108,16 +95,7 @@ function getTeamOverview(teamSearch) {
         // Clears all old content that may be present within the main content container
         $("#main-content").empty();
 
-        // Grabs team ID to use as the parameter for another AJAX call
-        var teamID = response.teams[0].id;
-
-        // Stores the team logo url, the team name, country of origin, and founding date
-        var logo = response.teams[0].logo;
-        var name = response.teams[0].name;
-        var debut = response.teams[0].debut;
-        var abbrev = response.teams[0].abbrev;
-
-        // Dynamically Adds in the div containers and sections for the statistics api
+        // Dynamically Adds in the div containers and sections for the All Teams api
 
         var bottomRightCard = $("<div>")
             .addClass("card-section small-6 secM")
@@ -132,15 +110,15 @@ function getTeamOverview(teamSearch) {
 
         // object shortcut variable
         var allTeams = response.teams;
-        console.log(allTeams);
+        // console.log(allTeams);
         // loops 18 times to get the 18 playing teams.
-        for (var i = 0; i <= 18; i++) {
-            var TeamName = allTeams[i].name;
-            var TeamDebut = allTeams[i].debut;
-            var TeamShortName = allTeams[i].abbrev;
+        for (var i = 0; i < 18; i++) {
+            var TeamName = 'Team Name :' + allTeams[i].name + ' ';
+            var TeamDebut = 'Team Debut :' + allTeams[i].debut + ' ';
+            //var TeamShortName = ' :' + allTeams[i].abbrev + '';
             var TeamInfoCol = $("<p style='font-size: 12px'>")
                 .addClass("lineup-col")
-                .text(TeamName + ", " + TeamDebut + ", " + TeamShortName + "")
+                .text(TeamName + ", " + TeamDebut + " ")
                 .appendTo($(".secM"));
         }
     });
